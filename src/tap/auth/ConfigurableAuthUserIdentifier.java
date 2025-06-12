0a859a731eb1141166fbc2bb7a189ae1ccf5f31c
Anthony Heng <anthony.heng@mq.edu.au>
2025-06-12 17:05:06 +1000

    Wrongly instantiated when should have assigned to object property

--- src/tap/auth/ConfigurableAuthUserIdentifier.java
+++ src/tap/auth/ConfigurableAuthUserIdentifier.java
@@ -179,7 +179,7 @@ public class ConfigurableAuthUserIdentifier implements UserIdentifier {
 		String propValue = tapConfig.getProperty(KEY_RESP_ALLOW_ANONYMOUS);
 		this.allowAnonymous = (propValue == null) ? false : Boolean.parseBoolean(propValue); // Default: do not support anonymous 
 		propValue = tapConfig.getProperty(KEY_API_TIMEOUT);
-		this.apiTimeout = (propValue == null) ? 5000 : Integer.parseInt(propValue); //set timeout to 5 seconds as default
+		int apiTimeout = (propValue == null) ? 5000 : Integer.parseInt(propValue); //set timeout to 5 seconds as default
 
 		try{
 			this.api = new JSONAPIClient(this.authURL, "GET", apiTimeout);
